51ce28c8d075b0be19ed8f5c2bf9e728fae88d34
cfalcon <cfalcon@cells.es>
2016-10-14 16:30:36 +0200

    Add setup.py
    Create setup.py based on setuptools.

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+##############################################################################
+##
+# This file is part of Taurus
+##
+# http://taurus-scada.org
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Taurus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Taurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+from setuptools import setup, find_packages
+import taurus
+
+version = '0.0.1'
+description = 'h5file Taurus scheme'
+long_description = ''' h5file is the Taurus scheme for manage hdf5 files.
+This first implementation offers a read-only scheme with basic interface to
+access to hdf5 files.'''
+license = taurus.Release.license
+platforms = taurus.Release.platforms
+
+authors = 'ctgensoft'
+email = 'ctgensoft@cells.es'
+url = 'http://www.taurus-scada.org/en/stable/devel/core_tutorial.html'
+
+install_requires = ['taurus>=4.0.1',
+                    'h5py']
+
+setup(name='h5file',
+      version=version,
+      description=description,
+      long_description=long_description,
+      author=authors,
+      maintainer=authors,
+      maintainer_email=email,
+      url=url,
+      platforms=platforms,
+      license=license,
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      test_suite='h5file.test.testsuite.get_suite',
+      )
